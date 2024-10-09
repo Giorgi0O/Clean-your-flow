8109d2c241401b25ec9ef41c8f57b1551cfd5072
Giorgio <giorgioft0@gmail.com>
2024-10-09 19:37:17 +0200

    Configuration for translation with file

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: true,
     fallbackLng: 'en',
+    debug: true,
     interpolation: {
-      escapeValue: false,
-    },
-    resources: {
-      en: {
-
-      },
+      escapeValue: false
     }
   });
 
