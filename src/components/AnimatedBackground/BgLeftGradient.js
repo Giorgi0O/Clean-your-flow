47eed414f1f6a5bb8a432fd537b26905a472f9a2
Giorgio <giorgioft0@gmail.com>
2024-10-13 11:59:29 +0200

    refactor : New versione of welcome page (ready for mobile)

--- src/components/AnimatedBackground/BgLeftGradient.js
+++ src/components/AnimatedBackground/BgLeftGradient.js
@@ -2,7 +2,7 @@ function BgLeftGradient() {
     
     return (
         <>
-            <div className='z-0 absolute w-full h-screen overflow-hidden'>
+            <div className={`z-10 absolute w-full h-screen overflow-hidden`}>
                 <div className='background-left-3-colors bg-rosa-opacity top-[-10%] left-[-15%] rotate-[20deg]'  ></div>
                 <div className='background-left-3-colors bg-verde-opacity top-[35%] left-[-20%]' ></div>
                 <div className='background-left-3-colors bg-ciano-opacity top-[70%] left-[-15%] rotate-[-20deg]' ></div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,35 +4,69 @@ import Title from '../components/Title/Title';
 import ContentBox from '../components/AppText/ContentBox';
 import SessionStart from '../components/Buttons/SessionStart'
 import { useTranslation } from 'react-i18next';
+import {ReactComponent as ModalIcon1} from '../assets/Icons/task_modal_icon.svg';
+import {ReactComponent as ModalIcon2} from '../assets/Icons/time_selector.svg';
+import {ReactComponent as ModalIcon3} from '../assets/Icons/mode_selector.svg';
+import {ReactComponent as Arrow} from '../assets/Icons/arrow.svg';
 
 function Home() {
 
   const { t } = useTranslation();
 
   return (
-    <div className='app'>
-        <BgLeftGradient/>
-        <div className='lg:fixed lg:left-0 center flex-col w-full h-full z-[100] lg:w-1/2 lg:flex-row ' >
-            <div className='flex-col text-center center h-full w-1/2'>
-              <div className='fixed top-4'>
-                <Title />
-              </div>
-              <ContentBox
-                  body={
-                      <>
-                        {t('home.ours_objective')}                  
-                      </>
-                  }
-                  bodyClass={'font-titolo text-2xl font-bold text-ciano-dark'}
-              />
-            </div>
-            <div className=' fixed bottom-0 lg:right-0
-              w-5/6 bg-ciano-opacity center p-4 rounded-full shadow-sm mb-2
-              lg:rounded-none lg:m-0 lg:center lg:h-full lg:w-1/2'
-            >
-                <SessionStart />
-            </div>
+    <div className='h-screen'>
+      <BgLeftGradient/>
+
+      <div className='z-[101] w-full fixed top-2 left-2/4 translate-x-[-50%]'>
+        <Title></Title>
+      </div>
+
+      <div className='z-[101] fixed bottom-4 left-2/4 translate-x-[-50%]'>
+        <SessionStart></SessionStart>
+        <p className='text-center z-[100] font-body font-bold text-sm mt-2 text-ciano-dark'> {t('home.scroll1')} </p>
+      </div>
+
+      <div className='relative top-[9%] lg:top-0 z-[100] w-screen h-5/6 lg:h-full overflow-y-scroll overflow-x-hidden'>
+        
+        <div className=' w-screen h-screen p-8 center flex-col text-center'>
+          <p className='font-titolo font-bold text-2xl text-ciano-dark'> {t('home.main1')} </p>
+          <p className='font-titolo font-bold text-2xl text-ciano-dark mb-8'> {t('home.main2')} </p>
+          <Arrow onClick={() => document.getElementById('second-section').scrollIntoView({ behavior: 'smooth' })} />
         </div>
+        
+        <div id='second-section' className='mb-[100px] lg:mb-0 center flex-col lg:flex-row w-screen h-[130vh] lg:h-screen justify-between lg:p-16'>
+
+          {/* card task */}
+          <div  className='card-mirror-ciano'> 
+            <div className='h-1/2 w-full center m-8'>
+              <ModalIcon1/>
+            </div>
+            <h1 className='font-titolo font-bold text-3xl text-ciano-dark'> {t('home.card1.title')} </h1>
+            <span className='font-body text-sm text-ciano'> {t('home.card1.body')} </span>
+          </div>
+
+          {/* card tempo */}
+          <div  className='card-mirror-verde'> 
+            <div className='h-1/2 w-full center m-8'>
+              <ModalIcon2/>
+            </div>
+            <h1 className='font-titolo font-bold text-3xl text-verde-dark'> {t('home.card2.title')} </h1>
+            <span className='font-body text-sm text-verde'> {t('home.card2.body')} </span>
+          </div>
+
+          {/* card modalità */}
+          <div  className='card-mirror-rosa'> 
+            <div className='h-1/2 w-full center m-8'>
+              <ModalIcon3/>
+            </div>
+            <h1 className='font-titolo font-bold text-3xl text-rosa-dark'> {t('home.card3.title')} </h1>
+            <span className='font-body text-sm text-rosa'> {t('home.card3.body')} </span>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
