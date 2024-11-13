7d40c39fe55882f49691559d292b7cfd08ee69f2
Giorgio <giorgioft0@gmail.com>
2024-11-13 23:06:03 +0100

    fix : Add SoundManager.js file

---
+++ src/utils/SoundManager.js
@@ -0,0 +1,63 @@
+
+class SoundManager {
+    constructor() {
+        this.sounds = {};
+        this.audioInstances = {};
+    }
+
+    init() {
+        const soundPaths = {
+            'click': '/sounds/start-click.wav',
+            'start-flow': '/sounds/start-flow.wav',
+            'start-flow-muted': '/sounds/start-flow.wav',
+            // Aggiungi altri suoni qui
+        };
+
+        Object.entries(soundPaths).forEach(([type, path]) => {
+            const audio = new Audio(path);
+            audio.preload = 'auto';
+
+            this.audioInstances[type] = Array(3).fill(null).map(() => {
+                const instance = new Audio(path);
+                if(type === 'start-flow-muted'){
+                    instance.muted = true;
+                }
+                instance.preload = 'auto';
+                return instance;
+            });
+        });
+    }
+
+    play(type) {
+        if (!this.audioInstances[type]) {
+            console.warn(`Sound type "${type}" not found.`);
+            return;
+        }
+
+        const instances = this.audioInstances[type];
+        const availableInstance = instances.find(instance => instance.paused || instance.ended) || instances[0];
+
+        availableInstance.currentTime = 0;
+
+        try {
+            const playPromise = availableInstance.play();
+            if (playPromise) {
+                playPromise.catch(error => {
+                    console.warn('Playback failed:', error);
+                });
+            }
+        } catch (error) {
+            console.warn('Playback failed:', error);
+        }
+    }
+
+}
+const soundManager = new SoundManager();
+
+export function playSound(type, options = {}) {
+    soundManager.play(type, options);
+}
+
+export function initSoundManager() {
+    soundManager.init();
+}
