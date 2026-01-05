70ef717dc8cd772eee14d7ffcc3ef060823a1aae
Giorgio <giorgioft0@gmail.com>
2026-01-05 11:07:29 +0100

    fix: correct variable name in FControls component

--- src/components/page-flow-session/session/FControls.js
+++ src/components/page-flow-session/session/FControls.js
@@ -1,40 +1,31 @@
-import React from 'react';
-import BStop from '../../shared/BStop'
-import BStart from '../../shared/BStart'
-import useNotifications from '../../../hooks/useNotifications'
+import BStop from "../../shared/BStop";
+import BStart from "../../shared/BStart";
+import useNotifications from "../../../hooks/useNotifications";
 
-function FControls({
-  isActive,
-  isflow,
-  flowmodoroStart,
-  flowmodoroBreath,
-}) {
-
+function FControls({ isActive, flow, flowmodoroStart, flowmodoroBreath }) {
   const notify = useNotifications();
 
   const handleStart = () => {
     notify.notifyFlowMuted();
     notify.notifyClick();
     flowmodoroStart();
-  }
+  };
 
   const handleBreath = () => {
     notify.notifyFlowMuted();
     notify.notifyClick();
     flowmodoroBreath();
-  }
+  };
 
   return (
-    <div className='w-5/6 text-center'>
-      {
-        !isflow ?
-          <BStart operation={handleStart} type={3} disabled={isActive} ></BStart>
-          :
-          <BStop operation={handleBreath} type={2} ></BStop>
-      }
+    <div className="w-5/6 text-center">
+      {!flow ? (
+        <BStart operation={handleStart} type={3} disabled={isActive}></BStart>
+      ) : (
+        <BStop operation={handleBreath} type={2}></BStop>
+      )}
     </div>
   );
-
 }
 
-export default FControls
+export default FControls;
